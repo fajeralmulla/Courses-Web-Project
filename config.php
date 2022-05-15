29fb5929d1b6d0c6b4a9629891ef534b345d0dd3
Fajer <59661642+fajeralmulla@users.noreply.github.com>
2022-05-15 14:07:10 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ROOT_URL', 'http://localhost/course+/'); //add your localhost link
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');//add your name
+define('DB_PASS', '????');
+define('DB_NAME', 'course+');
+?>
